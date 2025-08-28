2ef42b4b5c456b59bc009f8474152599e16e88fb
mguerra <marcelo.grra@gmail.com>
2025-08-27 22:51:14 -0300

    Ejercicios resueltos de operadores

---
+++ src/main/java/org/javafintech2025/IntoJava_Modulo1/Ejercicio5_Operadores.java
@@ -0,0 +1,40 @@
+package org.javafintech2025.IntoJava_Modulo1;
+
+/*
+Ejercicio 5: operador resto
+1. Analizar el código que sigue y completar la tabla correspondiente.
+2. Luego, realizar la codificación y confirmar que se ha completado correctamente la tabla.
+*/
+
+public class Ejercicio5_Operadores {
+    public static void main(String[] args) {
+        System.out.println('A');
+        int n1=20;
+        int n2=2;
+        int n3=n1%n2;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println('B');
+        n1=15;
+        n2=2;
+        n3=n1%n2;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println('C');
+        n1=3;
+        n2=20;
+        n3=n2%n1;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println('D');
+        n1=3;
+        n2=15;
+        n3=n2%n1;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+    }
+}

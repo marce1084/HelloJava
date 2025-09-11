05d7394ab573a70e81874a016a57b47f3426fd94
marce1084 <marcelo.grra@gmail.com>
2025-09-10 23:45:29 -0300

    Ejercicio 1 - tipo de variables

---
+++ src/main/java/org/javafintech2025/IntoJava_Modulo1/Ejercicio8_tipoVariable.java
@@ -0,0 +1,20 @@
+package org.javafintech2025.IntoJava_Modulo1;
+
+/*
+Ejercicio 1
+Si variableA, variableB y variableC son de tipo lógicas o boolean, ¿qué valores deben poseer para que las
+siguientes operaciones den los siguientes resultados?:
+● (variableA && variableB) || (variableA && variableC) = true
+● !(variableA || variableB) && variableC = false
+*/
+public class Ejercicio8_tipoVariable {
+    public static void main(String[] args) {
+        boolean variableA = true;
+        boolean variableB = false;
+        boolean variableC = true;
+
+        System.out.println("La varialbeA es true");
+        System.out.println("La variableB es false");
+        System.out.println("La variableC es true");
+    }
+}
